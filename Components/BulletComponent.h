659c72e0d9a9a06750737e159cd558993b77e0ca
Duncan Sparks <duncan.sprks@gmail.com>
2020-02-22 09:55:48 -0800

    Improve macro readability

--- Components/BulletComponent.h
+++ Components/BulletComponent.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "Component.h"
+#include "Vector2.h"
 
 class BulletComponent : public Component {
 private:
 	float speed = 500;
+	Vector2 velocity = {0, 0};
 
 public:
 	float get_speed() { return speed; }
--- Core/ECSSystem.h
+++ Core/ECSSystem.h
@@ -2,10 +2,11 @@
 
 #include <cstdio>
 
-#define ECS_PRINT_MESSAGE(Fmt, ...) std::fprintf(stdout, "\033[0m[\033[0;32mECS\033[0m] " Fmt "\n", __VA_ARGS__)
-#define ECS_PRINT_WARNING(Fmt, ...) std::fprintf(stderr, "\033[0m[\033[0;32mECS\033[0m]\033[0;35m WARNING: " Fmt "\033[0m\n", __VA_ARGS__)
-#define ECS_PRINT_ERROR(Fmt, ...) std::fprintf(stderr, "\033[0m[\033[0;32mECS\033[0m]\033[0;31m ERROR: " Fmt "\033[0m\n", __VA_ARGS__)
+#define __GREEN "\033[0;32m"
+#define __RED "\033[0;31m"
+#define __MAGENTA "\033[1;35m"
+#define __RESET "\033[0m"
 
-namespace ecs {
-	
-}
+#define ECS_PRINT_MESSAGE(Fmt, ...) std::fprintf(stdout, __RESET "[" __GREEN "ECS" __RESET "] " Fmt "\n", __VA_ARGS__)
+#define ECS_PRINT_WARNING(Fmt, ...) std::fprintf(stderr, __RESET "[" __GREEN "ECS" __RESET "] " __MAGENTA "WARNING: " Fmt __RESET "\n", __VA_ARGS__)
+#define ECS_PRINT_ERROR(Fmt, ...) std::fprintf(stderr, __RESET "[" __GREEN "ECS" __RESET "] " __RED "ERROR: " Fmt __RESET "\n", __VA_ARGS__)
